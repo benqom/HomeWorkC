86a166f2a0bfc5344f8297c1f02b5d7302e500a1
Alexii <Alexii@vavilov.ru>
2023-03-24 20:09:28 +0300

    code task8

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear(); // очистка консоли
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i=1;
+while(n>=i)
+{
+  if ((i % 2) == 0) 
+  Console.WriteLine(i);   
+  i++;
+}
+
+
+ 
