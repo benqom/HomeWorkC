0602681b9ca03b48b089e65ed01567608a953d02
Alexii <Alexii@vavilov.ru>
2023-03-24 21:46:03 +0300

    code task23

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите  число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i=1;
+while (i<=a)
+{Console.WriteLine(i*i*i);
+ i++;
+}
+
