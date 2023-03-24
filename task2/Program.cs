af62e6a37f90a2fb604eae996bb06a1192f100e5
Alexii <Alexii@vavilov.ru>
2023-03-24 19:37:17 +0300

    code task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear(); // очистка консоли
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) 
+ Console.WriteLine("max="+ a);
+ else
+ Console.WriteLine("max="+b);
