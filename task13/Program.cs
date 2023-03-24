416bdb8726c02259737b775a8c475767516b491b
Alexii <Alexii@vavilov.ru>
2023-03-24 21:14:41 +0300

    code task13

---
+++ task13/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите 3-х значное число: ");
+string a = Console.ReadLine();
+if (a.Length<3)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine(a[1]);
