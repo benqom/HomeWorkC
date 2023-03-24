b1f9170aac1f74420eb022050823e2bbd4a8a616
Alexii <Alexii@vavilov.ru>
2023-03-24 21:28:22 +0300

    code task19

---
+++ task19/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите 5-х значное число: ");
+string a = Console.ReadLine();
+if ((a[0] ==a[4]) & (a[1]==a[3]) )
+Console.WriteLine("да");
+else
+Console.WriteLine("нет");
+
