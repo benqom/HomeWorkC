6a294f059738b3d39c375d9955e0130a8b0d31a5
Alexii <Alexii@vavilov.ru>
2023-03-30 21:57:57 +0300

    done task36

---
+++ task36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 999); // [100; 999]
+}
+
+int CountNoChetSumElement(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((i % 2) != 0)
+            count+=array[i]; 
+    }
+    return count;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях {CountNoChetSumElement(array)}");
+//Console.WriteLine($"Сумма отрицательных элементов равна {SumNegativeNumbers(array)}");
