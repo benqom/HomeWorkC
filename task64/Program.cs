cc8957f5c2d9eca9ca17ce44378c6af647dbaa00
Alexii <Alexii@vavilov.ru>
2023-04-05 16:57:45 +0300

    done  task64

---
+++ task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void f (int n)
+{
+  if (n == 0)  return;
+  Console.Write("{0,4}",n);  
+  f(n - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+f(n);
+//Console.WriteLine(f(n));
+/*
+f(5) -> f(4) + "5 "
+          |
+          f(3) + "4 "
+            |
+            f(2) + "3 "
+              |
+              f(1) + "2 "
+                |
+                "1 "
+*/
